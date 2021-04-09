0ba563a8dae7d3404e4fa25cd9de44fa9da828f7
kevin-fer <74706736+kevin-fer@users.noreply.github.com>
2021-04-09 14:21:13 +0200

    Page suivi commande fonctionne mais pas terminée

--- projet-sage-ecommerce/Controllers/HomeController.cs
+++ projet-sage-ecommerce/Controllers/HomeController.cs
@@ -143,21 +143,72 @@ namespace projet_sage_ecommerce.Controllers
             // Zone principale 
             ViewData["sitedevente"] = json.GetValue("SOH0_1").SelectToken("SALFCY"); // Site de vente
             ViewData["numcommande"] = json.GetValue("SOH0_1").SelectToken("SOHNUM"); // Numéro de la commande
-            //ViewData["datecommande"] = json.GetValue("SOH0_1").SelectToken("ORDDAT"); // Date de la commande
-            //ViewData["codeclient"] = json.GetValue("SOH0_1").SelectToken("BPCORD"); // Num client
+            DateTime date = new DateTime(Int32.Parse(json.GetValue("SOH0_1").SelectToken("ORDDAT").ToString().Substring(0,4)), Int32.Parse(json.GetValue("SOH0_1").SelectToken("ORDDAT").ToString().Substring(4, 2)), Int32.Parse(json.GetValue("SOH0_1").SelectToken("ORDDAT").ToString().Substring(6, 2)));
+            ViewData["datecommande"] = date.ToString().Substring(0, 10); // Date de la commande
+            ViewData["codeclient"] = json.GetValue("SOH0_1").SelectToken("BPCORD"); // Num client
             // État de la commande
-            //ViewData["etatcommande"] = json.GetValue("SOH1_5").SelectToken("ORDSTA_LBL"); // État de la commande
-            //ViewData["facturation"] = json.GetValue("SOH1_5").SelectToken("INVSTA_LBL"); // État de la facturation
+            ViewData["etatcommande"] = json.GetValue("SOH1_5").SelectToken("ORDSTA_LBL"); // État de la commande
+            ViewData["facturation"] = json.GetValue("SOH1_5").SelectToken("INVSTA_LBL"); // État de la facturation
             // Fournisseur
-            //ViewData["fournisseur"] = json.GetValue("SOH2_1").SelectToken("STOFCY"); // Fournisseur
-            // Livraison | Information transporteur
-            //ViewData["transporteurnum"] = json.GetValue("SOH2_3").SelectToken("BPTNUM"); // Id du transporteur
-            //ViewData["transporteurnom"] = json.GetValue("SOH2_3").SelectToken("ZBPTNUM"); // Nom du transporteur
-            //ViewData["transporteurnom"] = json.GetValue("SOH2_3").SelectToken("ZBPTNUM");
+            ViewData["fournisseur"] = json.GetValue("SOH2_1").SelectToken("STOFCY"); // Fournisseur
+            ViewData["fournisseurnom"] = json.GetValue("SOH2_1").SelectToken("ZSTOFCY"); // Nom du fournisseur
+            ViewData["priorite"] = json.GetValue("SOH2_1").SelectToken("DLVPIO_LBL"); // Priorité de la livraison
+
+            //DEVIS 
+            ViewData["numdevis"] = json.GetValue("SOH3_3").SelectToken("SQHNUM"); // Fournisseur
+            //Articles
+            JArray jsonArray = (JArray)json.GetValue("SOH4_1");//Get list of items
+
+            int e = 0;
+            foreach (JObject jsonObject in jsonArray)
+            {
+                CAdxModel tempC = new CAdxModel();
+                tempC.WsAlias = "WSYITM";
+                tempC.Param[0] = new CAdxParamKeyValue();
+                tempC.Param[0].key = "ITMREF";
+                tempC.Param[0].value = (string)jsonObject.SelectToken("ITMREF");
+                tempC.readObject();
+                JObject jsonTemp = JObject.Parse(tempC.Resultat.resultXml);
+                ViewData["blob" + e.ToString()] = jsonTemp.GetValue("ITM1_7").SelectToken("IMG");
+
+                ViewData["ITMREF" + e.ToString()] = jsonObject.SelectToken("ITMREF"); // id de l'article
+                ViewData["ITMDES" + e.ToString()] = jsonObject.SelectToken("ITMDES"); // designation
+                ViewData["QTY" + e.ToString()] = jsonObject.SelectToken("QTY"); // quantité
+                ViewData["GROPRI" + e.ToString()] = jsonObject.SelectToken("GROPRI"); // prix unitaire
+                ViewData["LINORDNOT" + e.ToString()] = jsonObject.SelectToken("LINORDNOT"); // total ligne
+                e++;
+            }
+            ViewData["length"] = e;
+            //Prix
+            ViewData["prixttht"] = json.GetValue("SOH4_4").SelectToken("ORDINVNOT"); // Prix total HT
+            ViewData["prixttTTC"] = json.GetValue("SOH4_4").SelectToken("ORDINVATI"); // Prix total TTC
+
+            JArray jsonArray1 = (JArray)json.GetValue("SOH3_5");//Get list of items
+            JObject jobj = (JObject)jsonArray1[0];
+            JObject jobj1 = (JObject)jsonArray1[2];
+            ViewData["remise"] = jobj.GetValue("INVDTAAMT"); // Remise %
+            ViewData["assurance"] = jobj1.GetValue("INVDTAAMT"); // Assurance %
+
+            //Livraison | Information transporteur
+            ViewData["transporteurnum"] = json.GetValue("SOH2_3").SelectToken("BPTNUM"); // Id du transporteur
+            ViewData["transporteurnom"] = json.GetValue("SOH2_3").SelectToken("ZBPTNUM"); // Nom du transporteur
             //ViewData["modedelivraison"] = json.GetValue("SOH2_3").SelectToken("MDL");
-            //ViewData["modedelivraisonnom"] = json.GetValue("SOH2_3").SelectToken("ZMDL");
-            //ViewData["livraisonnum"] = json.GetValue("SOH2_4").SelectToken("LASDLVNUM"); //Numéro de la livraison
-            //ViewData["livraisondate"] = json.GetValue("SOH2_4").SelectToken("LASDLVDAT");
+            ViewData["modedelivraisonnom"] = json.GetValue("SOH2_3").SelectToken("ZMDL");
+            ViewData["livraisonnum"] = json.GetValue("SOH2_4").SelectToken("LASDLVNUM"); //Numéro de la livraison
+            DateTime date1 = new DateTime(Int32.Parse(json.GetValue("SOH2_4").SelectToken("LASDLVDAT").ToString().Substring(0, 4)), Int32.Parse(json.GetValue("SOH2_4").SelectToken("LASDLVDAT").ToString().Substring(4, 2)), Int32.Parse(json.GetValue("SOH2_4").SelectToken("LASDLVDAT").ToString().Substring(6, 2)));
+            ViewData["livraisondate"] = date1.ToString().Substring(0, 10); // Date de la livraison
+            
+            ViewData["douane"] = json.GetValue("SOH2_3").SelectToken("ZEECICT"); //Douane
+            ViewData["tournee"] = json.GetValue("SOH2_3").SelectToken("DRN_LBL"); //tournee
+            ViewData["partielle"] = json.GetValue("SOH2_6").SelectToken("DME_LBL"); //partielle
+            DateTime date2 = new DateTime(Int32.Parse(json.GetValue("SOH2_2").SelectToken("DEMDLVDAT").ToString().Substring(0, 4)), Int32.Parse(json.GetValue("SOH2_2").SelectToken("DEMDLVDAT").ToString().Substring(4, 2)), Int32.Parse(json.GetValue("SOH2_2").SelectToken("DEMDLVDAT").ToString().Substring(6, 2)));
+            ViewData["livraisondatedemandee"] = date2.ToString().Substring(0, 10); // Date de la livraison demandée
+            
+            DateTime date3 = new DateTime(Int32.Parse(json.GetValue("SOH2_2").SelectToken("SHIDAT").ToString().Substring(0, 4)), Int32.Parse(json.GetValue("SOH2_2").SelectToken("SHIDAT").ToString().Substring(4, 2)), Int32.Parse(json.GetValue("SOH2_2").SelectToken("SHIDAT").ToString().Substring(6, 2)));
+            ViewData["livraisondateexpedition"] = date3.ToString().Substring(0, 10); // Date d'expedition
+
+            ViewData["delai"] = json.GetValue("SOH2_2").SelectToken("DAYLTI"); // délai prévu
+
             // Adresse 
             /*ViewData["adpays"] = json.GetValue("ADB2_1").SelectToken("ZCRY"); //Pays
             JArray jsonArray = (JArray)json.GetValue("ADB2_1").SelectToken("BPAADDLIG");//Adresse
@@ -169,6 +220,7 @@ namespace projet_sage_ecommerce.Controllers
                 e++;
             }
             */
+
             json = JObject.Parse(c.Resultat.resultXml);
 
             return View("SuiviCommande");
